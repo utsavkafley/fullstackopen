33d6397b183c25a9c1f4bbf885ddfc9bf5cc019a
vastukafley@gmail.com <vastukafley@gmail.com>
2022-01-03 20:44:23 -0500

    anecdotes step3 || Finished with all exercises for part1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
 
+const DisplayAnecdote = ({ anecdote, votes }) => {
+  if (!anecdote) {
+    return <p>No anecdotes have been voted yet</p>;
+  }
+
+  return (
+    <p>
+      {anecdote} <br /> has {votes} votes
+    </p>
+  );
+};
 const App = () => {
   const anecdotes = [
     "If it hurts, do it more often",
@@ -10,22 +21,39 @@ const App = () => {
     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
   ];
-
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const [mostVoted, setMostVoted] = useState(null);
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-  const handleClick = () => {
+  const handleClickNextButton = () => {
     let selected = getRandomInt(anecdotes.length - 1);
     setSelected(selected);
   };
 
+  const handleClickVoteButton = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+
+    setMostVoted(copy.indexOf(Math.max(...copy)));
+  };
+
   return (
     <div>
-      {anecdotes[selected]} <br />
-      <button onClick={handleClick}>next anecdote</button>
+      <h1>Anecdote of the day</h1>
+      <DisplayAnecdote anecdote={anecdotes[selected]} votes={votes[selected]} />
+      <button onClick={handleClickNextButton}>next anecdote</button>
+      <button onClick={handleClickVoteButton}>vote</button>
+
+      <h1>Anecdote with most votes</h1>
+      <DisplayAnecdote
+        anecdote={anecdotes[mostVoted]}
+        votes={votes[mostVoted]}
+      />
     </div>
   );
 };

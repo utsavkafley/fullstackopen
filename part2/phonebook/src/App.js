85920559f4841672ae980e28abbf78186d80aab3
Utsav Kafley <vastukafley@gmail.com>
2022-01-23 17:53:39 -0500

    Part 2 || Phonebook step 12 || All exercises finished for part 2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import Notification from "./components/Notification";
+import Error from "./components/Error";
 import phonebookService from "./services/phonebook";
 
 const App = () => {
@@ -10,6 +12,8 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchString, setSearchString] = useState("");
+  const [notificationMessage, setNotificationMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     phonebookService.getAll().then((response) => setPersons(response));
@@ -17,52 +21,65 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault();
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
 
-    if (persons.some((person) => person.name === newName)) {
-      window.alert(`${newName} is already added to phonebook, replace the old number with a new one?`);
-
-      const id = persons.find(person => person.name === newName).id
-      const personObject = {
-        name: newName,
-        number: newNumber,
-      };
-      phonebookService.update(id, personObject).then(response => {
-        let newPersons = persons.filter(
-          (person) => person.id !== id
-        )
-        setPersons(newPersons.concat(response))
-      })
-
-    } else if (!newName || !newNumber) {
+    if (!newName || !newNumber) {
       window.alert("Please fill out all fields");
-    } else {
-      const personObject = {
-        name: newName,
-        number: newNumber,
-      };
+    } else if (persons.some((person) => person.name === newName)) {
+      const id = persons.find((person) => person.name === newName).id;
 
-      phonebookService
-        .create(personObject)
-        .then((response) => setPersons(persons.concat(response)));
-
-      Array.from(document.querySelectorAll("input")).forEach(
-        (input) => (input.value = "")
+      window.alert(
+        `${newName} is already added to phonebook, replace the old number with a new one?`
       );
 
-      setNewName("");
-      setNewNumber("");
+      phonebookService
+        .update(id, personObject)
+        .then((response) => {
+          let newPersons = persons.filter((person) => person.id !== id);
+          setPersons(newPersons.concat(response));
+          setNotificationMessage(
+            `${newName}'s information has been updated in the phonebook`
+          );
+        })
+        .catch((error) => {
+          setErrorMessage(
+            `${newName}'s information has already been deleted from the server`
+          );
+          setPersons(persons.filter((n) => n.id !== id));
+        });
+    } else {
+      phonebookService.create(personObject).then((response) => {
+        setPersons(persons.concat(response));
+        setNotificationMessage(`${newName} has been added to the phonebook`);
+      });
     }
+
+    Array.from(document.querySelectorAll("input")).forEach(
+      (input) => (input.value = "")
+    );
+
+    setNewName("");
+    setNewNumber("");
+    setTimeout(() => {
+      setNotificationMessage(null);
+      setErrorMessage(null);
+    }, 3000);
   };
 
   const removePerson = (id) => {
     window.alert("Are you sure you want to remove this entry from phonebook?");
     phonebookService.remove(id).then(() => {
-      let newPersons = persons.filter(
-        (person) => person.id !== id
-      )
+      let newPersons = persons.filter((person) => person.id !== id);
 
       setPersons(newPersons);
+      setNotificationMessage(`Entry has been removed from the phonebook`);
     });
+    setTimeout(() => {
+      setNotificationMessage(null);
+    }, 3000);
   };
 
   const filterHandler = (event) => {
@@ -70,7 +87,10 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className="container">
+      <Notification message={notificationMessage} />
+      <Error message={errorMessage} />
+
       <h2>Phonebook</h2>
       <Filter searchString={searchString} filterHandler={filterHandler} />
 
